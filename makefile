3b30c08a86b705f5634a11afd88a9fa577e85bca
krishnat17 <krishnathevathasan@gmail.com>
2016-11-07 00:09:09 -0500

    all but sync

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 all: threads
 
 threads: threads.c
-	gcc -pthread threads.c -o threads
+	gcc -pthread threads.c -w -o threads
 
 clean:
 	rm -f threads
--- threads.c
+++ threads.c
@@ -5,20 +5,26 @@
 
 int number_of_servers = 2;
 int number_of_clients = 2;
-int total_clock_ticks = 1000;
+int totalTicks = 1;
 
 double muX = 0.01;
 double *mu = &muX;
-double lambdaX = 0.05;
+double lambdaX = 0.005;
 double *lambda = &lambdaX;
 
-int globalCurrentTick;
-int globalPrevTick;
+int currentTick;
 
 
 pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t server_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t cond_tick_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+pthread_cond_t waiting_for_tick_cond = PTHREAD_COND_INITIALIZER;
+
+
+double RNG() {
+  return (double)rand() / (double)RAND_MAX;
+}
 
 struct job {
   struct job *next;
@@ -40,25 +46,24 @@ void enq() {
     temp -> next = NULL;
     rear = temp;
   }
-  printf("Job enq successful.");
 }
 
 void deq() {
   front1 = front;
 
   if (front1 == NULL) {
-    printf("\n ERROR: Trying to deq an empty queue.");
+    printf("ERROR: Trying to deq an empty queue.\n");
     return;
   }
   else {
       if (front1 -> next != NULL) {
         front1 = front1 ->   next;
-        printf("Job deq successful.");
+        printf("Job deq successful.\n");
         free(front);
         front = front1;
       }
       else {
-        printf("Job deq successful.");
+        printf("Job deq successful.\n");
         free(front);
         front = rear = NULL;
       }
@@ -72,91 +77,90 @@ int qIsEmpty() {
       return 0;
 }
 
-void *myfunc(void *myvar) {
-  char *msg;
-  msg = (char*) myvar;
-
-  int i;
-  for (i=0;i<10;i++) {
-    printf("%s %d\n", msg, i);
-    sleep(1);
-  }
-  return 0;
-}
-
 void *server_sim(void *mu) {
 
-  double *deathrate;
-  deathrate = (double*) mu;
+  double* moo;
+  moo = (double*) mu;
 
   int jobExecutingBool = 0;
+  double deathrate = *moo;
   double tempRand;
 
   //Seed RNG in this thread
   srand(time(0));
 
-  while (globalCurrentTick < total_clock_ticks) {
+  while (currentTick < totalTicks) {
+
     //Check if currently working on job
     if (jobExecutingBool == 0) {
-      if(qIsEmpty != 1) {
-        pthread_mutex_lock(&server_mutex);
-        deq();
-        pthread_mutex_unlock(&server_mutex);
+
+      if(qIsEmpty() == 0) {
+        tempRand = RNG();
+        printf("%f\n",tempRand);
+
+        if(tempRand < deathrate){
+          jobExecutingBool = 0;
+          printf("Finished job this tick\n");
+        }
+        else {
+          printf("Did not finish job this tick\n");
+        }
+      }
+      else {
+        printf("No job to work on this tick\n");
       }
     }
     else {
-      tempRand = RNG();
-
-      if(tempRand < deathrate){
-        jobExecutingBool = 0;
-      }
-
+      pthread_mutex_lock(&server_mutex);
+      deq();
+      pthread_mutex_unlock(&server_mutex);
     }
-
+    pthread_cond_wait(&waiting_for_tick_cond,&cond_tick_mutex);
   }
+  pthread_exit(NULL);
 
-  return 0;
 }
 
 void *client_sim(void *lambda) {
 
-  double *birthrate;
-  birthrate = (double*) lambda;
+  double* lam;
+  lam = (double*) lambda;
 
+  double birthrate = *lam;
   double tempRand;
 
   //Seed RNG in this thread
   srand(time(0));
 
-  while (globalCurrentTick < total_clock_ticks) {
-
+  while (currentTick < totalTicks) {
     tempRand = RNG();
+    printf("%f\n",tempRand);
 
     if(tempRand < birthrate) {
       pthread_mutex_lock(&client_mutex);
       enq();
       pthread_mutex_unlock(&client_mutex);
+      printf("Did create job this tick\n");
     }
-
+    else {
+      printf("Did not create job this tick\n");
+    }
+    pthread_cond_wait(&waiting_for_tick_cond,&cond_tick_mutex);
   }
-
+  pthread_exit(NULL);
 }
 
 void *clock_sim(void *unused) {
 
-  globalCurrentTick = 0;
-  globalPrevTick = 0;
+  currentTick = 0;
 
-  while(globalCurrentTick < total_clock_ticks) {
-    globalCurrentTick++;
-
+  while(currentTick < totalTicks) {
+    sleep(5);
+    pthread_cond_broadcast(&waiting_for_tick_cond);
+    currentTick++;
+    printf("--------------Current tick is %d\n",currentTick);
   }
-
-  return 0;
-}
-
-double RNG() {
-  return (double)rand() / (double)RAND_MAX;
+  pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
@@ -169,7 +173,6 @@ int main(int argc, char *argv[]) {
   pthread_t client_thread1, client_thread2;
   pthread_t clock_thread;
 
-
   pthread_create(&server_thread1, NULL, server_sim, (void*) mu);
   pthread_create(&server_thread2, NULL, server_sim, (void*) mu);
 
@@ -178,31 +181,14 @@ int main(int argc, char *argv[]) {
 
   pthread_create(&clock_thread, NULL, clock_sim, NULL);
 
+  //Main waits for threads to finish
+  pthread_join(server_thread1, NULL);
+  pthread_join(server_thread2, NULL);
+  pthread_join(client_thread1, NULL);
+  pthread_join(client_thread2, NULL);
+  pthread_join(clock_thread, NULL);
 
-
-
-  //Begin test section
-
-  pthread_t thread1, thread2;
-  char* msg1 = "First thread";
-  char* msg2 = "Second thread";
-
-  int ret1, ret2;
-
-  ret1 = pthread_create(&thread1, NULL, myfunc, (void*) msg1);
-  ret2 = pthread_create(&thread2, NULL, myfunc, (void*) msg2);
-
-  printf("Main function after pthread_create\n");
-
-  pthread_join(thread1,NULL);
-  pthread_join(thread2,NULL);
-
-  printf("Main function after pthread_join\n");
-
-  printf("First thread ret1 = %d\n", ret1);
-  printf("Second thread ret2 = %d\n", ret2);
-
-  //End test section
+  printf("Back in main\n");
 
   return 0;
 }
